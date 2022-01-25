6af6db15c13197ec6577f5900d953e6e4d8cf1e8
Xee <191440042@qq.com>
2022-01-25 17:29:58 +0800

    update onnx

--- include/onnx_graph_simplifier.hpp
+++ include/onnx_graph_simplifier.hpp
@@ -4,38 +4,23 @@
 
 // Copyright (C) 2020, Intel Corporation, all rights reserved.
 // Third party copyrights are property of their respective owners.
-
 #ifndef __OPENCV_DNN_ONNX_SIMPLIFIER_HPP__
 #define __OPENCV_DNN_ONNX_SIMPLIFIER_HPP__
-
 #include "precomp.hpp"
 
-#if defined(__GNUC__) && __GNUC__ >= 5
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wsuggest-override"
-#endif
 #include "opencv-onnx.pb.h"
-#if defined(__GNUC__) && __GNUC__ >= 5
-#pragma GCC diagnostic pop
-#endif
+namespace cv
+{   namespace dnn{
 
-namespace cv { namespace dnn {
-CV__DNN_INLINE_NS_BEGIN
-
-void simplifySubgraphs(opencv_onnx::GraphProto& net);
-
-template<typename T1, typename T2>
-void convertInt64ToInt32(const T1& src, T2& dst, int size)
-{
-    for (int i = 0; i < size; i++)
+    void simplifySubgraphs(opencv_onnx::GraphProto &net);
+    template <typename T1, typename T2>
+    void convertInt64ToInt32(const T1 &src, T2 &dst, int size)
     {
-        dst[i] = saturate_cast<int32_t>(src[i]);
+        for (int i = 0; i < size; i++)
+        {
+            dst[i] = cv::saturate_cast<int>(src[i]);
+        }
     }
-}
-
-Mat getMatFromTensor(opencv_onnx::TensorProto& tensor_proto);
-
-CV__DNN_INLINE_NS_END
-}}  // namespace dnn, namespace cv
-
-#endif  // __OPENCV_DNN_ONNX_SIMPLIFIER_HPP__
+    cv::Mat getMatFromTensor(const opencv_onnx::TensorProto &tensor_proto);
+}}
+#endif
